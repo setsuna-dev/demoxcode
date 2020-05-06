edca2020dbec1d8e32113c5a89400fc57606a290
setsuna-dev <62680067+setsuna-dev@users.noreply.github.com>
2020-05-06 11:09:51 +0200

    Initial Commit

---
+++ demoxcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demoxcode
+//
+//  Created by rigal nicolas on 06/05/2020.
+//  Copyright © 2020 rigal nicolas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
